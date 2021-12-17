7634bd8f49cab050e0ca88e63f8d7a3bcf36af7c
ithinkitskevin <jaehwa96.lee@Gmail.com>
2021-12-16 18:12:08 -0800

    rehabilitation methods bug fix

--- functions/add_acute_dose.m
+++ functions/add_acute_dose.m
@@ -1,3 +1,3 @@
 function add_acute_dose = add_acute_dose()
-    add_acute_dose = [repmat(225, [1,21]), repmat(75, [1,42]), zeros(1, 365-21-42), repmat(225, [1, 42]), zeros(1, 21) ];
+    add_acute_dose = [repmat(225, [1,21]), repmat(300, [1,42]), zeros(1, 365) ];
 end
--- functions/add_chronic_dose.m
+++ functions/add_chronic_dose.m
@@ -1,3 +1,3 @@
 function add_chronic_dose = add_chronic_dose()
-    add_chronic_dose = [repmat(225, [1,21]), repmat(300, [1,42]), zeros(1, 365) ];
+    add_chronic_dose = [repmat(225, [1,21]), repmat(75, [1,42]), zeros(1, 365-21-42), repmat(225, [1, 42]), zeros(1, 21) ];
 end
